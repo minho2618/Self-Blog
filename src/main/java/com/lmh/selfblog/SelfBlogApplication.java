fd3e3205134b572b3a9d10b6191cebf2553d878a
minho2618 <48886903+minho2618@users.noreply.github.com>
2024-07-09 23:54:17 +0900

    Initial commit

---
+++ src/main/java/com/lmh/selfblog/SelfBlogApplication.java
@@ -0,0 +1,13 @@
+package com.lmh.selfblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lmh/selfblog/SelfBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmh.selfblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
