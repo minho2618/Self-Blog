8ac8d917c0140d5592bd1775fa62611eb3501b03
minho2618 <minho2618@naver.com>
2024-08-07 00:02:48 +0900

    블로그 글 추가 API 추가

--- src/main/java/com/lmh/selfblog/Repository/BlogRepository.java
+++ src/main/java/com/lmh/selfblog/Repository/BlogRepository.java
@@ -2,6 +2,8 @@ package com.lmh.selfblog.Repository;
 
 import com.lmh.selfblog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository<Article, Long> {
 }
